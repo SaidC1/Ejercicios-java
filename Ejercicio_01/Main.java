8111f2b8bb558eee50e61fe43f36730212b6dae5
Irvin Said <txpro123@hotmail.com>
2025-09-22 23:01:39 -0600

    Add files via upload

---
+++ Ejercicio_01/Main.java
@@ -0,0 +1,19 @@
+package Ejercicios_java.Ejercicio_01;
+
+public class Main {
+    public static void main(String[]args){
+        // Imprimir en pantalla
+        System.out.println("Hola, mundo");
+
+        // Declaración de variables
+        int edad = 18;
+        double promedio = 8.5;
+        String nombre = "Irvin";
+
+        // Imprimir variables
+        System.out.println("Edad: " + edad);
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Nombre: " + nombre);
+    }
+    
+}
