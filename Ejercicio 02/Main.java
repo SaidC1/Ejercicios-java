03df0b27328d27c4b665848a3d566bbab7b7e9ef
Irvin Said <txpro123@hotmail.com>
2025-09-25 23:54:25 -0600

    Add files via upload

--- Ejercicio 02/Main.java
+++ Ejercicio 02/Main.java
@@ -1,24 +1,19 @@
+package Ejercicios_java.Ejercicio_01;
+
 public class Main {
     public static void main(String[]args){
+        // Imprimir en pantalla
+        System.out.println("Hola, mundo");
+
         // Declaración de variables
-        int numero1 = 10;
-        int numero2 = 3;
+        int edad = 18;
+        double promedio = 8.5;
+        String nombre = "Irvin";
 
-        // Operaciones aritméticas
-        int suma = numero1 + numero2;
-        int resta = numero1 - numero2;
-        int multiplicacion = numero1 * numero2;
-        int division = numero1 / numero2; // División entera
-        int modulo = numero1 % numero2; // Resto de la división
-
-        // Imprimir resultados
-        System.out.println("Numero 1: " + numero1);
-        System.out.println("Numero 2: " + numero2);
-        System.out.println("Suma: " + suma);
-        System.out.println("Resta: " + resta);
-        System.out.println("Multiplicación: " + multiplicacion);
-        System.out.println("División: " + division);
-        System.out.println("Módulo: " + modulo);
-
-        }
+        // Imprimir variables
+        System.out.println("Edad: " + edad);
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Nombre: " + nombre);
+    }
+    
 }
