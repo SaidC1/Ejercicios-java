5317b72ba418bd3e2ce27979e376f8a84085cc5e
Irvin Said <txpro123@hotmail.com>
2025-09-25 23:54:07 -0600

    Add files via upload

---
+++ Ejercicio 02/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[]args){
+        // Declaración de variables
+        int numero1 = 10;
+        int numero2 = 3;
+
+        // Operaciones aritméticas
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        int division = numero1 / numero2; // División entera
+        int modulo = numero1 % numero2; // Resto de la división
+
+        // Imprimir resultados
+        System.out.println("Numero 1: " + numero1);
+        System.out.println("Numero 2: " + numero2);
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
+        System.out.println("Módulo: " + modulo);
+
+        }
+}
