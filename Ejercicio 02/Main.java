5b941dfdd05088797676a4eeaaeea0d7177e58e9
Irvin Said <txpro123@hotmail.com>
2025-09-28 20:16:40 -0600

    Update Main.java

--- Ejercicio 02/Main.java
+++ Ejercicio 02/Main.java
@@ -1,19 +1,40 @@
-package Ejercicios_java.Ejercicio_01;
+package Ejercicios_java.Ejercicio_02;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[]args){
-        // Imprimir en pantalla
-        System.out.println("Hola, mundo");
+    public static void main(String[] args) {
+        Scanner Lector = new Scanner (System.in);
+        
+        // Pedir dos numeros enteros
+        System.out.println("Ingrese el primer numero entero:");
+        int num1 = Lector.nextInt();
 
-        // Declaración de variables
-        int edad = 18;
-        double promedio = 8.5;
-        String nombre = "Irvin";
+        System.out.println("Ingrese el segundo numero entero:");
+        int num2 = Lector.nextInt();
 
-        // Imprimir variables
-        System.out.println("Edad: " + edad);
-        System.out.println("Promedio: " + promedio);
-        System.out.println("Nombre: " + nombre);
+        // Operaciones aritmeticas
+        int suma = num1 + num2;
+        int resta = num1 - num2;
+        int multiplicacion = num1 * num2;
+        double division = (double) num1 / num2;
+        int modulo = num1 % num2;
+
+        // Validar para evitar division por cero
+        if (num2 != 0) {
+            division = num1 / num2;
+            modulo = num1 % num2;
+        } else {
+            System.out.println("Error: Division por cero no es permitida.");
+        }
+
+        // Mostrar resultados
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicacion: " + multiplicacion);
+        if (num2 != 0) {
+            System.out.println("Division: " + division);
+            System.out.println("Modulo: " + modulo);
+        }
+        Lector.close();
     }
-    
 }
